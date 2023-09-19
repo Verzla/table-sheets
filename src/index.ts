53b66ec2da55563a094fb508a72d40ee2dd717f9
Adam Viðarsson <adam@igital.co>
2023-09-19 07:45:51 +0000

    More exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * from './types/Grid';
-export * from './components/Grid';
+export { Grid } from './components/Grid';
+export { useGridContext } from './components/GridContext';
+export { Autocomplete } from './components/autocomplete/Autocomplete';
+export { Input } from './components/input/Input';
